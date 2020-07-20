450bd66f120e81777d0173ee3403bd159b6702df
KarlosWins <>
2020-07-20 11:13:22 +0200

    remove jquery from popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,27 +1,22 @@
 var browser = chrome;
 
 browser.storage.local.get(['ignoredUsers', 'checkboxHidePosts', 'checkboxHideThreads', 'checkboxFixNightWolf'], function (result) {
-  $("#ignoredUsers").val(result.ignoredUsers);
-  $("#checkboxHidePosts").prop("checked", (result.checkboxHidePosts));
-  $("#checkboxHideThreads").prop("checked", (result.checkboxHideThreads));
-  $("#checkboxFixNightWolf").prop("checked", (result.checkboxFixNightWolf));
+  ignoredUsers.value = result.ignoredUsers;
+  checkboxHidePosts.checked = result.checkboxHidePosts;
+  checkboxHideThreads.checked = result.checkboxHideThreads;
+  checkboxFixNightWolf.checked = result.checkboxFixNightWolf;
 });
 
-$("#ignoredUsers").change(function () {
-  browser.storage.local.set({ 'ignoredUsers': this.value })
-});
+document.getElementById('ignoredUsers').onchange = function () {
+  browser.storage.local.set({ 'ignoredUsers': ignoredUsers.value })
+};
 
-$("#checkboxHidePosts").change(function () {
-  let value = $("#checkboxHidePosts").prop('checked');
-  browser.storage.local.set({ 'checkboxHidePosts': value })
-});
-
-$("#checkboxHideThreads").change(function () {
-  let value = $("#checkboxHideThreads").prop('checked');
-  browser.storage.local.set({ 'checkboxHideThreads': value })
-});
-
-$("#checkboxFixNightWolf").change(function () {
-  let value = $("#checkboxFixNightWolf").prop('checked');
-  browser.storage.local.set({ 'checkboxFixNightWolf': value })
-}); 
+document.getElementById('checkboxHidePosts').onchange = function () {
+  browser.storage.local.set({ 'checkboxHidePosts': checkboxHidePosts.checked })
+};
+document.getElementById('checkboxHideThreads').onchange = function () {
+  browser.storage.local.set({ 'checkboxHideThreads': checkboxHideThreads.checked })
+};
+document.getElementById('checkboxFixNightWolf').onchange = function () {
+  browser.storage.local.set({ 'checkboxFixNightWolf': checkboxFixNightWolf.checked })
+}; 
