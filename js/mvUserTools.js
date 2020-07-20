65c55bdc7c848db0ffc6df8f1a1ef664fc5ca326
KarlosWins <>
2020-07-20 11:14:09 +0200

    refactor variables

--- js/mvUserTools.js
+++ js/mvUserTools.js
@@ -1,17 +1,17 @@
-var browser = chrome;
+let browser = chrome;
 
 browser.storage.local.get(['ignoredUsers', 'checkboxHidePosts', 'checkboxHideThreads', 'checkboxFixNightWolf'], function (userSettings) {
 
   // Ocultar posts
   if (userSettings.checkboxHidePosts) {
-    var x = document.getElementsByClassName("cf post");
-    for (var i = 0; i < x.length; i++) {
+    let x = document.getElementsByClassName("cf post");
+    for (let i = 0; i < x.length; i++) {
       if (userSettings.ignoredUsers.includes(x[i].dataset.autor)) {
         let postNumber = x[i].dataset.num;
-        var hiddenGroup = document.createElement("div");
+        let hiddenGroup = document.createElement("div");
         hiddenGroup.classList.add("moderated-group");
-        var a = $('<a name="' + i + 1 + '" class="name-pad"></a>');
-        var hiddenPostCard = $('<div class="post info"><div class="post-meta"><a href="#' + postNumber + '" class="qn">#' + postNumber + '</a><div class="post-body"><div class="locked-msg"><a class="post-btn hiddenmsg" href="#post-' + postNumber + '"><i class="fa fa-flag"></i> Has ocultado este post de <b>' + x[i].dataset.autor + '</b>. Pulsa para mostrar el post</a></div></div></div></div>');
+        let a = $('<a name="' + i + 1 + '" class="name-pad"></a>');
+        let hiddenPostCard = $('<div class="post info"><div class="post-meta"><a href="#' + postNumber + '" class="qn">#' + postNumber + '</a><div class="post-body"><div class="locked-msg"><a class="post-btn hiddenmsg" href="#post-' + postNumber + '"><i class="fa fa-flag"></i> Has ocultado este post de <b>' + x[i].dataset.autor + '</b>. Pulsa para mostrar el post</a></div></div></div></div>');
         x[i].classList.add('locked');
         $(hiddenGroup).append(a);
         $(hiddenGroup).append(hiddenPostCard);
@@ -22,9 +22,9 @@ browser.storage.local.get(['ignoredUsers', 'checkboxHidePosts', 'checkboxHideThr
 
   // Ocultar hilos
   if (userSettings.checkboxHideThreads) {
-    var threads = document.getElementsByClassName("tooltip-left");
-    for (var i = 0; i < threads.length; i++) {
-      var nombreUsuario = threads[i].href.split("/");
+    let threads = document.getElementsByClassName("tooltip-left");
+    for (let i = 0; i < threads.length; i++) {
+      let nombreUsuario = threads[i].href.split("/");
       nombreUsuario = nombreUsuario[nombreUsuario.length - 1];
       if (userSettings.ignoredUsers.includes(nombreUsuario)) {
         threads[i].parentNode.parentNode.style.display = "none";
@@ -36,12 +36,12 @@ browser.storage.local.get(['ignoredUsers', 'checkboxHidePosts', 'checkboxHideThr
   // Fix nick Nightwolf
   
   if (userSettings.checkboxFixNightWolf) {
-    var nicks = document.getElementsByClassName("autor user-card");
-    for (var i = 0; i < nicks.length; i++) {
-      var nombreUsuario = nicks[i].href.split("/");
-      nombreUsuario = nombreUsuario[nombreUsuario.length - 1];
-      if (nombreUsuario == "NigthWolf") {
-        nicks[i].textContent = "NightWolf";
+    let autores = document.getElementsByClassName("autor user-card");
+    for (let i = 0; i < autores.length; i++) {
+      let userURL = autores[i].href.split("/");
+      let username = userURL[userURL.length - 1];
+      if (username == "NigthWolf") {
+        autores[i].textContent = "NightWolf";
       }
     }
   }
